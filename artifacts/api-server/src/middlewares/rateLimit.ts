b53b2ef0d3f269818e4b5f626d46e67b3cadd2ef
patidar1102 <gouravptdr@gmail.com>
2026-07-12 14:09:28 +0530

    Change import statement for rateLimit

--- artifacts/api-server/src/middlewares/rateLimit.ts
+++ artifacts/api-server/src/middlewares/rateLimit.ts
@@ -1,4 +1,4 @@
-import rateLimit from "express-rate-limit";
+import { rateLimit } from "express-rate-limit";
 import type { Request } from "express";
 import { classifyUserAgent } from "../lib/botDetection";
 
